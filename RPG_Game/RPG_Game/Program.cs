5e433243a914b3fb9d2e2d1d4e209aa78c7d9fff
Artem8279065278956333338637498569828 <artemgorban07@gmail.com>
2025-12-02 12:10:50 +0200

    create Character

--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -1,5 +1,12 @@
 ﻿namespace RPG_Game
 {
+    public abstract class Character
+    {
+        private int _health;
+        private int _maxHealth;
+        private int _strenght;
+        private string _name;
+    }
     internal class Program
     {
         static void Main(string[] args)
